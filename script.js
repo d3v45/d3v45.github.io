97b9f351c169ad3eed76dd8abdcdc6671c483627
Devadathan P. R <devadathanparambath123@gmail.com>
2024-07-16 14:26:44 +0530

    Added files

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ document.querySelectorAll('nav a').forEach(anchor => {
         // Close the mobile menu after clicking a link
         if (window.innerWidth <= 768) {
             navLinks.classList.remove('show');
+            menuIcon.classList.remove('active');
+            backdrop.classList.remove('show');
         }
     });
 });
@@ -18,10 +20,19 @@ document.querySelectorAll('nav a').forEach(anchor => {
 // Toggle menu for mobile
 const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
+const backdrop = document.getElementById('backdrop');
 
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('show');
     menuIcon.classList.toggle('active');
+    backdrop.classList.toggle('show');
+});
+
+// Close the menu when clicking on the backdrop
+backdrop.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+    menuIcon.classList.remove('active');
+    backdrop.classList.remove('show');
 });
 
 // Intersection Observer for animations
